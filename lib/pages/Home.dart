d16859a983092bcdf9e2638c53bf69e670e4d160
비엔나 <sure@paran.com>
2023-09-13 15:53:10 +0900

    test : design structure

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -41,6 +41,7 @@ class _HomeState extends State<Home> {
             ),
             Chart(title: 'oo'),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(onPressed: (){
                   showDialog(context: context, builder: (context) => AddBox(title: 'oo'));
